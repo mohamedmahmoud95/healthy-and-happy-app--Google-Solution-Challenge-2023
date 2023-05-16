7cf9e0408a9551c5ac75d05c56e036c2cad90c3a
David Wagih <davidwagih62@gmail.com>
2023-05-16 08:59:43 +0300

    add packages and files for connecting with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'Constants/project_colors.dart';
 import 'screens/login_screen/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
