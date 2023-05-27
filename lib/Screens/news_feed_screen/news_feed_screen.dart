8c24791a1122b1f8abc59fed191ae3294a3a9875
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-05-27 18:48:29 +0300

    added methods to load image from gallery, camera, or an image url, to use to create a new post

--- lib/Screens/news_feed_screen/news_feed_screen.dart
+++ lib/Screens/news_feed_screen/news_feed_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:core';
-
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mental_health_app/Constants/project_colors.dart';
 import 'package:mental_health_app/Models/appUser.dart';
 import 'package:mental_health_app/Models/category.dart';
@@ -8,6 +11,7 @@ import 'package:mental_health_app/Models/post.dart';
 import 'package:mental_health_app/Reusable%20Widgets/button_widget.dart';
 import 'package:mental_health_app/Reusable%20Widgets/post_card.dart';
 import '../../Reusable Widgets/category_card.dart';
+import 'package:path_provider/path_provider.dart';
 
 class NewsFeedScreen extends StatefulWidget {
   const NewsFeedScreen({super.key});
@@ -25,6 +29,7 @@ class _NewsFeedScreenState extends State<NewsFeedScreen> {
   PostCategory selectedCategory = trending;
 
 
+
   @override
   void initState() {
     super.initState();
@@ -38,18 +43,56 @@ class _NewsFeedScreenState extends State<NewsFeedScreen> {
   }
 
 
-  // File? image;
-  //
-  // Future pickImage() async {
-  //   try {
-  //     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-  //     if(image == null) return;
-  //     final imageTemp = File(image.path);
-  //     setState(() => this.image = imageTemp);
-  //   } on PlatformException catch(e) {
-  //     print('Failed to pick image: $e');
-  //   }
-  // }
+  File? image;
+
+  Future pickImageFromGallery() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if(image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch(e) {
+      debugPrint('Failed to pick image: $e');
+    }
+  }
+
+
+  Future imageFromCamera() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      if(image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch(e) {
+      debugPrint('Failed to pick image: $e');
+    }
+  }
+
+
+  String? enteredImageUrl;
+
+  void imageFromUrl(String imageUrl) async {
+    try {
+      final response = await http.get(Uri.parse(imageUrl));
+      if (response.statusCode == 200) {
+        final bytes = response.bodyBytes;
+        final tempDir = await getTemporaryDirectory();
+        final tempPath = '${tempDir.path}/temp_image.jpg';
+        final tempFile = File(tempPath);
+        await tempFile.writeAsBytes(bytes);
+
+        setState(() {
+          image = tempFile;
+        });
+      } else {
+        debugPrint('Failed to load image. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      debugPrint('Failed to load image: $e');
+    }
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -213,41 +256,92 @@ class _NewsFeedScreenState extends State<NewsFeedScreen> {
                                           context: context,
                                           builder: (context) {
                                             return Container(
-                                              height: 150,
-                                              child: Column(
-                                                children: [
-                                                  ListTile(
-                                                    leading: const Icon(
-                                                      Icons.link,
-                                                      color: mainPurple,
+                                             // height: 150,
+                                              child: SingleChildScrollView(
+                                                child: Column(
+                                                  children: [
+
+                                                     ExpansionTile(
+                                                      leading: const Icon(
+                                                        Icons.link,
+                                                        color: mainPurple,
+                                                      ),
+                                                      title: const Text(
+                                                          'Copy Image URL',
+                                                          style: TextStyle(
+                                                              color: mainPurple)),
+                                                      controlAffinity: ListTileControlAffinity.leading,
+                                                      children: <Widget>[
+                                                        TextField(
+                                                          style:  const TextStyle(color: Colors.black),
+                                                          onChanged: (value) {
+                                                            enteredImageUrl = value;
+                                                          },
+                                                          decoration: InputDecoration(
+                                                            hintText: "Paste image URL here",
+                                                            border: OutlineInputBorder(
+                                                              borderRadius: BorderRadius.circular(10),
+                                                            ),
+                                                            focusedBorder: const OutlineInputBorder(
+                                                              borderSide: BorderSide(color: lavender),
+                                                            ),
+                                                          ),
+                                                          maxLines: 1,
+                                                        ),
+                                                        IconButton(onPressed: (){
+                                                          imageFromUrl(enteredImageUrl!);
+                                                        }, icon: Icon(Icons.upload_sharp)),
+                                                      ],
                                                     ),
-                                                    title: const Text(
-                                                        'Copy Image URL',
+
+                                                    const SizedBox(
+                                                        width: 400,
+                                                        child: Divider(
+                                                          thickness: 1,
+                                                        )),
+                                                    ListTile(
+                                                      leading: const Icon(
+                                                        Icons.file_upload,
+                                                        color: mainPurple,
+                                                      ),
+                                                      title: const Text(
+                                                        'Upload from Device',
                                                         style: TextStyle(
-                                                            color: mainPurple)),
-                                                    onTap: () {},
-                                                  ),
-                                                  const SizedBox(
-                                                      width: 400,
-                                                      child: Divider(
-                                                        thickness: 1,
-                                                      )),
-                                                  ListTile(
-                                                    leading: const Icon(
-                                                      Icons.file_upload,
-                                                      color: mainPurple,
+                                                            color: mainPurple),
+                                                      ),
+                                                      onTap: () {
+                                                        pickImageFromGallery();
+                                                        Navigator.pop(context);
+                                                      },
                                                     ),
-                                                    title: const Text(
-                                                      'Upload from Device',
-                                                      style: TextStyle(
-                                                          color: mainPurple),
+
+                                                    const SizedBox(
+                                                        width: 400,
+                                                        child: Divider(
+                                                          thickness: 1,
+                                                        )),
+                                                    ListTile(
+                                                      leading: const Icon(
+                                                        Icons.camera_alt_outlined,
+                                                        color: mainPurple,
+                                                      ),
+                                                      title: const Text(
+                                                        'Take a picture',
+                                                        style: TextStyle(
+                                                            color: mainPurple),
+                                                      ),
+                                                      onTap: () {
+                                                        imageFromCamera();
+                                                        Navigator.pop(context);
+                                                      },
                                                     ),
-                                                    onTap: () {
-                                                      // Perform action for uploading from device
-                                                      Navigator.pop(context);
-                                                    },
-                                                  ),
-                                                ],
+
+                                                    const SizedBox(
+                                                        height: 10,
+                                                      ),
+
+                                                  ],
+                                                ),
                                               ),
                                             );
                                           },
@@ -318,6 +412,11 @@ class _NewsFeedScreenState extends State<NewsFeedScreen> {
                                                   Navigator.pop(context);
                                                 },
                                               ),
+
+                                              const SizedBox(
+                                                height: 10,
+                                              ),
+
                                             ],
                                           ),
                                         );
