802c7454f247da0a69f2184552de00ef111ad715
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-06-03 01:30:09 +0300

    edited the appointment and AppUser models (to prevent stack overflow do to infinit cycle cuz appuser has appointment and each appointment has an appUser), and made some UI updates

--- lib/Models/appUser.dart
+++ lib/Models/appUser.dart
@@ -1,3 +1,5 @@
+import 'package:mental_health_app/Models/appointment.dart';
+
 class AppUser {
   String? id;
   String? email;
@@ -8,10 +10,11 @@ class AppUser {
   String? profilePicUrl;
   String? about;
   String? lastActive;
-  List<String>? idsOfAnotherUsersChattedWith=[];
-  List<String>? likedPostsIds=[];
-  List<String>? friendsUsersIds=[];
+  List<String>? idsOfAnotherUsersChattedWith = [];
+  List<String>? likedPostsIds = [];
+  List<String>? friendsUsersIds = [];
   bool isTherapist;
+  List<String>? bookedAppointmentsIDs = [];
 
   AppUser({
     this.id,
@@ -26,15 +29,67 @@ class AppUser {
     this.idsOfAnotherUsersChattedWith,
     this.friendsUsersIds,
     this.likedPostsIds,
-    this.isTherapist = false, //false by default, unless the opposite is explicitly declared
+    this.isTherapist =
+        false, //false by default, unless the opposite is explicitly declared
+    this.bookedAppointmentsIDs,
   });
-
 }
 
+AppUser sampleAppUser1 = AppUser(
+    firstName: "Mohamed",
+    lastName: "Mahmoud",
+    email: "m.raslan97@gmail.com",
+    phoneNumber: "+201001412578",
+    about: "حد علوضعه جدًا",
+    lastActive: "3 min ago",
+    password: '12345678',
+    profilePicUrl: "assets/images/Mohamed_Mahmoud.png",
+    bookedAppointmentsIDs: ['sampleAppointment1', 'sampleAppointment2', 'sampleAppointment5'],
 
-AppUser sampleAppUser1 = AppUser(firstName: "Mohamed", lastName: "Mahmoud",  email: "m.raslan97@gmail.com",            phoneNumber: "+201001412578", about: "حد علوضعه جدًا", lastActive: "3 min ago", password: '12345678', profilePicUrl: "assets/images/Mohamed_Mahmoud.png");
-AppUser sampleAppUser2 = AppUser(firstName: "Esraa",   lastName: "Amr",      email: "Esraaamrkandil@gmail.com",        phoneNumber: "+20123456789",  about: "..",             lastActive: "1 hr ago",  );
-AppUser sampleAppUser3 = AppUser(firstName: "Habiba",  lastName: "Alaa",     email: "https://github.com/Habibaaahmed", phoneNumber: "+20123456789",  about: "..",             lastActive: "3 hr ago",  );
-AppUser sampleAppUser4 = AppUser(firstName: "Sondos",  lastName: "",         email: "https://github.com/S0nd0s",       phoneNumber: "+20123456789",  about: "..",             lastActive: "1 day ago", );
+);
 
-AppUser thisAppUser = sampleAppUser1; // a test appUser to use as the current appUser (the one using the app from his phone)
+AppUser sampleAppUser2 = AppUser(
+  firstName: "Esraa",
+  lastName: "Amr",
+  email: "Esraaamrkandil@gmail.com",
+  phoneNumber: "+20123456789",
+  about: "..",
+  lastActive: "1 hr ago",
+  bookedAppointmentsIDs: [],
+
+);
+AppUser sampleAppUser3 = AppUser(
+  firstName: "Habiba",
+  lastName: "Alaa",
+  email: "https://github.com/Habibaaahmed",
+  phoneNumber: "+20123456789",
+  about: "..",
+  lastActive: "3 hr ago",
+  bookedAppointmentsIDs: [],
+
+);
+AppUser sampleAppUser4 = AppUser(
+  firstName: "Sondos",
+  lastName: "",
+  email: "https://github.com/S0nd0s",
+  phoneNumber: "+20123456789",
+  about: "..",
+  lastActive: "1 day ago",
+  bookedAppointmentsIDs: [],
+
+);
+
+AppUser thisAppUser =
+AppUser(
+  firstName: "Mohamed",
+  lastName: "Mahmoud",
+  email: "m.raslan97@gmail.com",
+  phoneNumber: "+201001412578",
+  about: "حد علوضعه جدًا",
+  lastActive: "3 min ago",
+  password: '12345678',
+  profilePicUrl: "assets/images/Mohamed_Mahmoud.png",
+  bookedAppointmentsIDs: ['sampleAppointment1', 'sampleAppointment2', 'sampleAppointment5'],
+
+);
+ //   sampleAppUser1; // a test appUser to use as the current appUser (the one using the app from his phone)
--- lib/Screens/sessions_screen_for_therapists/tabs/tab/scheduleTab.dart
+++ lib/Screens/sessions_screen_for_therapists/tabs/tab/scheduleTab.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_app/Constants/project_colors.dart';
 import 'package:mental_health_app/Screens/sessions_screen_for_therapists/page/widget/BookingPage.dart';
-
 import '../../../../Models/appointment.dart';
 import '../widget/dateTimeCard.dart';
 
@@ -13,15 +12,22 @@ class ScheduleTab extends StatefulWidget {
 }
 
 class _ScheduleTabState extends State<ScheduleTab> {
-  Status status = Status.upcoming;
+  Status selectedStatus = Status.upcoming;
   Alignment _alignment = Alignment.centerLeft;
   bool showCancelButton = false;
   bool showRescheduleButton = false;
 
   @override
+  void initState() {
+    super.initState();
+    showCancelButton = selectedStatus == Status.upcoming;
+    showRescheduleButton = selectedStatus == Status.upcoming;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List<Appointment> filteredSchedules = listOfSampleAppointments.where((var schedule) {
-      return schedule.status == status;
+    List<Appointment> filteredSchedules = listOfSampleAppointments.where((schedule) {
+      return schedule.status == selectedStatus;
     }).toList();
 
     void cancelSchedule(int index) {
@@ -30,15 +36,9 @@ class _ScheduleTabState extends State<ScheduleTab> {
       });
     }
 
-    @override
-    void initState() {
-      super.initState();
-      showCancelButton = status == Status.upcoming;
-      showRescheduleButton = status == Status.upcoming;
-    }
-
     return Scaffold(
-      backgroundColor: mainWhite,
+
+    backgroundColor: mainWhite,
       appBar: AppBar(
         backgroundColor: mainWhite,
         elevation: 0,
@@ -80,7 +80,8 @@ class _ScheduleTabState extends State<ScheduleTab> {
                           child: GestureDetector(
                             onTap: () {
                               setState(() {
-                                status = status;
+                                selectedStatus = status;
+                                debugPrint("status = $status");
                                 if (status == Status.upcoming) {
                                   _alignment = Alignment.centerLeft;
                                   showCancelButton = true;
@@ -118,7 +119,7 @@ class _ScheduleTabState extends State<ScheduleTab> {
                     ),
                     child: Center(
                       child: Text(
-                        status.name,
+                        selectedStatus.name,
                         style: const TextStyle(
                           color: mainWhite,
                           fontWeight: FontWeight.bold,
@@ -196,7 +197,8 @@ class _ScheduleTabState extends State<ScheduleTab> {
                               if (showCancelButton)
                                 Expanded(
                                   child: OutlinedButton(
-                                    child: const Text('Cancel'),
+
+                                      child: const Text('Cancel', style: TextStyle(fontSize: 16, color: mainPurple),),
                                     onPressed: () {
                                       cancelSchedule(index);
                                     },
@@ -209,7 +211,11 @@ class _ScheduleTabState extends State<ScheduleTab> {
                               if (showRescheduleButton)
                                 Expanded(
                                   child: ElevatedButton(
-                                    child: const Text('Reschedule'),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: mainPurple
+
+                  ),
+                                    child: const Text('Reschedule', style: TextStyle(fontSize: 16),),
                                     onPressed: () {
                                       Navigator.push(
                                         context,
